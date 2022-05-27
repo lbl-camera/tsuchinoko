359359850ddeafc91f2a4ae160b90cc439d62907
ronpandolfi <ronpandolfi@gmail.com>
2022-05-27 11:07:17 -0700

    add metrics to data injection

--- tsuchinoko/adaptive/__init__.py
+++ tsuchinoko/adaptive/__init__.py
@@ -1,6 +1,5 @@
-from dataclasses import dataclass, field
-
-import numpy as np
+from dataclasses import dataclass, field, asdict
+from collections import defaultdict
 
 
 @dataclass()
@@ -9,12 +8,33 @@ class Data:
     positions: list = field(default_factory=list)
     scores: list = field(default_factory=list)
     variances: list = field(default_factory=list)
+    metrics: dict = field(default_factory=dict)
 
     def inject_new(self, data):
         for datum in data:
             self.positions.append(datum[0])
             self.scores.append(datum[1])
             self.variances.append(datum[2])
+            for metric in datum[3]:  # TODO: handle logical cases
+                if metric not in self.metrics:
+                    self.metrics[metric] = []
+                self.metrics[metric].append(datum[3][metric])
+
+    as_dict = asdict
+
+    def __getitem__(self, item:slice):
+        print(item)
+        return Data(self.dimensionality, self.positions[item], self.scores[item], self.variances[item], {key: value[item] for key, value in self.metrics.items()})
+
+    def __len__(self):
+        return len(self.positions)
+
+    def extend(self, data: 'Data'):
+        self.positions += data.positions
+        self.scores += data.scores
+        self.variances += data.variances
+        for key in self.metrics:
+            self.metrics[key] += data[key]
 
 
 class Engine:
