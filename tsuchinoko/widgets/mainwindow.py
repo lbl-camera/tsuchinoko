9e07b5f93d1acb71a0243a252696a7e96cfd30f0
ronpandolfi <ronpandolfi@gmail.com>
2022-05-27 11:09:32 -0700

    add zmq client

--- tsuchinoko/widgets/mainwindow.py
+++ tsuchinoko/widgets/mainwindow.py
@@ -1,8 +1,16 @@
+import time
+
+from loguru import logger
+import numpy as np
+from PySide2.QtCore import QTimer
 from PySide2.QtGui import QIcon
+from pyqtgraph import PlotWidget, ScatterPlotItem, CurveArrow, TextItem, mkBrush, mkColor, mkPen
 from pyqtgraph.dockarea import DockArea
 from qtmodern.styles import dark
 from qtpy.QtWidgets import QMainWindow, QApplication
 
+from tsuchinoko.adaptive import Data
+from tsuchinoko.utils.threads import method, invoke_in_main_thread, iterator, QThreadFutureIterator
 from tsuchinoko.widgets.displays import Log, Configuration, RunEngineControls, GraphManager
 
 
@@ -12,10 +20,11 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle('Tsuchinoko')
         self.setWindowIcon(QIcon('assets/tsuchinoko.png'))
+        self.resize(1700, 1000)
 
         self.log_widget = Log()
         self.configuration_widget = Configuration()
-        self.run_engine_widget = RunEngineControls()
+        # self.run_engine_widget = RunEngineControls()
         self.graph_manager_widget = GraphManager()
 
         self.dock_area = DockArea()
@@ -24,16 +33,98 @@ class MainWindow(QMainWindow):
         for position, w, *relaltive_to in [('bottom', self.graph_manager_widget),
                                            ('bottom', self.log_widget, self.graph_manager_widget),
                                            ('right', self.configuration_widget, self.graph_manager_widget),
-                                           ('bottom', self.run_engine_widget, self.configuration_widget),
+                                           # ('bottom', self.run_engine_widget, self.configuration_widget),
                                            ]:
             self.dock_area.addDock(w, position, *relaltive_to)
 
         dark(QApplication.instance())
 
-    @property
-    def experiment(self):
-        return self.run_engine_widget.experiment
+        import zmq
+        context = zmq.Context()
 
-    @experiment.setter
-    def experiment(self, value):
-        self.run_engine_widget.experiment = value
+        #  Socket to talk to server
+        print("Connecting to core server…")
+        self.socket = context.socket(zmq.REQ)
+        self.socket.connect("tcp://localhost:5555")
+
+        self.update_thread = QThreadFutureIterator(self.update, yield_slot=self.update_graphs)
+        self.update_thread.start()
+
+    def update(self):
+        data = None
+        import json, zmq
+
+        while True:
+            if data:
+                message = f'partial_data {len(data)}'.encode()
+            else:
+                message = b"full_data"
+
+            try:
+                logger.info(f'request: {message}')
+                self.socket.send(message)
+                #  Get the reply.
+                message = self.socket.recv()
+            except zmq.ZMQError:
+                pass
+            else:
+
+                # print("Received reply [ %s ]" % (message))
+
+                if data:
+                    data.extend(Data(**json.loads(message)))
+                else:
+                    data = Data(**json.loads(message))
+                yield data
+
+    def init_graph(self, name, indicator='maxvalue'):
+        if name not in self.graph_manager_widget.graphs:
+            graph = PlotWidget()
+            scatter = ScatterPlotItem(name='scatter', x=[0], y=[0], size=10, pen=mkPen(None), brush=mkBrush(255, 255, 255, 120))
+
+            graph.addItem(scatter)
+            if indicator:
+                arrow = CurveArrow(scatter)
+                text = TextItem()
+                graph.addItem(arrow)
+                graph.addItem(text)
+
+            def _update_graph(data, indicator='maxvalue'):
+                if name == 'score':
+                    v = data.scores
+                elif name == 'variance':
+                    v = data.variances
+                else:
+                    v = data.metrics[name]
+
+                x, y = zip(*data.positions)
+
+                c = [255 * i / len(x) for i in range(len(x))]
+                max_index = np.argmax(v)
+
+                scatter.setData(
+                    [{'pos': (xi, yi),
+                      'size': (vi - min(v)) / (max(v) - min(v)) * 20 + 2 if max(v) != min(v) else 20,
+                      'brush': mkBrush(color=mkColor(255, 255, 255)) if i == len(x) - 1 else mkBrush(
+                          color=mkColor(255 - c, c, 0)),
+                      'symbol': '+' if i == len(x) - 1 else 'o'}
+                     for i, (xi, yi, vi, c) in enumerate(zip(x, y, v, c))])
+
+                arrow.setIndex(max_index)
+                text.setText(f'Max: {v[max_index]:.2f} ({x[max_index]:.2f}, {y[max_index]:.2f})')
+                text.setPos(x[max_index], y[max_index])
+
+            self.graph_manager_widget.register_graph(name, graph, _update_graph)
+
+    def update_graphs(self, data):
+        for metric_name in ['score', 'variance', *data.metrics]:
+            self.init_graph(metric_name)
+
+        self.graph_manager_widget.update(data)
+        # x, y = zip(*data.positions)
+        #
+        # for metric_name in data.metrics:
+        #     self._update_graph(metric_name, x, y, data.metrics[metric_name])
+        #
+        # self._update_graph('score', x, y, data.scores)
+        # self._update_graph('variance', x, y, data.variances)
