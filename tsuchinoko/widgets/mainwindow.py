b4e66428ed79420aba9ea5261c4841e05cd05442
ronpandolfi <ronpandolfi@gmail.com>
2022-06-29 13:34:04 -0700

    Add clouditem viz; add client timeout and reconnect

--- tsuchinoko/widgets/mainwindow.py
+++ tsuchinoko/widgets/mainwindow.py
@@ -10,6 +10,8 @@ from qtmodern.styles import dark
 from qtpy.QtWidgets import QMainWindow, QApplication
 
 from tsuchinoko.adaptive import Data
+from tsuchinoko.graphics_items.clouditem import CloudItem
+from tsuchinoko.graphics_items.indicatoritem import BetterCurveArrow
 from tsuchinoko.utils.threads import method, invoke_in_main_thread, iterator, QThreadFutureIterator
 from tsuchinoko.widgets.displays import Log, Configuration, RunEngineControls, GraphManager
 
@@ -39,6 +41,13 @@ class MainWindow(QMainWindow):
 
         dark(QApplication.instance())
 
+        self.init_socket()
+
+        self.update_thread = QThreadFutureIterator(self.update, yield_slot=self.update_graphs)
+        self.update_thread.start()
+
+
+    def init_socket(self):
         import zmq
         context = zmq.Context()
 
@@ -46,9 +55,7 @@ class MainWindow(QMainWindow):
         print("Connecting to core server…")
         self.socket = context.socket(zmq.REQ)
         self.socket.connect("tcp://localhost:5555")
-
-        self.update_thread = QThreadFutureIterator(self.update, yield_slot=self.update_graphs)
-        self.update_thread.start()
+        self.socket.RCVTIMEO = 5000
 
     def update(self):
         data = None
@@ -65,8 +72,11 @@ class MainWindow(QMainWindow):
                 self.socket.send(message)
                 #  Get the reply.
                 message = self.socket.recv()
-            except zmq.ZMQError:
-                pass
+            except zmq.ZMQError as ex:
+                logger.exception(ex)
+                self.init_socket()
+                data = None  # wipeout data and get a full update next time
+                time.sleep(1)
             else:
 
                 # print("Received reply [ %s ]" % (message))
@@ -80,44 +90,59 @@ class MainWindow(QMainWindow):
     def init_graph(self, name, indicator='maxvalue'):
         if name not in self.graph_manager_widget.graphs:
             graph = PlotWidget()
-            scatter = ScatterPlotItem(name='scatter', x=[0], y=[0], size=10, pen=mkPen(None), brush=mkBrush(255, 255, 255, 120))
+            # scatter = ScatterPlotItem(name='scatter', x=[0], y=[0], size=10, pen=mkPen(None), brush=mkBrush(255, 255, 255, 120))
+            cloud = CloudItem(name='scatter', size=10)
 
-            graph.addItem(scatter)
+            graph.addItem(cloud)
             if indicator:
-                arrow = CurveArrow(scatter)
+                max_arrow = BetterCurveArrow(cloud.scatter, brush=mkBrush('r'))
+                last_arrow = BetterCurveArrow(cloud.scatter, brush=mkBrush('w'))
                 text = TextItem()
-                graph.addItem(arrow)
-                graph.addItem(text)
+                graph.addItem(max_arrow)
+                # graph.addItem(text)
 
             def _update_graph(data, indicator='maxvalue'):
-                if name == 'score':
-                    v = data.scores
-                elif name == 'variance':
-                    v = data.variances
-                else:
-                    v = data.metrics[name]
+                with data:
+                    if name == 'score':
+                        v = data.scores
+                    elif name == 'variance':
+                        v = data.variances
+                    else:
+                        v = data.metrics[name]
 
-                x, y = zip(*data.positions)
+                    x, y = zip(*data.positions)
 
-                c = [255 * i / len(x) for i in range(len(x))]
+                # c = [255 * i / len(x) for i in range(len(x))]
                 max_index = np.argmax(v)
 
-                scatter.setData(
-                    [{'pos': (xi, yi),
-                      'size': (vi - min(v)) / (max(v) - min(v)) * 20 + 2 if max(v) != min(v) else 20,
-                      'brush': mkBrush(color=mkColor(255, 255, 255)) if i == len(x) - 1 else mkBrush(
-                          color=mkColor(255 - c, c, 0)),
-                      'symbol': '+' if i == len(x) - 1 else 'o'}
-                     for i, (xi, yi, vi, c) in enumerate(zip(x, y, v, c))])
+                cloud.setData(x=x,
+                              y=y,
+                              c=v,
+                              data=v,
+                              # size=5,
+                              hoverable=True,
+                              # hoverSymbol='s',
+                              # hoverSize=6,
+                              hoverPen=mkPen('b', width=2),
+                              # hoverBrush=mkBrush('g'),
+                              )
+                # scatter.setData(
+                #     [{'pos': (xi, yi),
+                #       'size': (vi - min(v)) / (max(v) - min(v)) * 20 + 2 if max(v) != min(v) else 20,
+                #       'brush': mkBrush(color=mkColor(255, 255, 255)) if i == len(x) - 1 else mkBrush(
+                #           color=mkColor(255 - c, c, 0)),
+                #       'symbol': '+' if i == len(x) - 1 else 'o'}
+                #      for i, (xi, yi, vi, c) in enumerate(zip(x, y, v, c))])
 
-                arrow.setIndex(max_index)
-                text.setText(f'Max: {v[max_index]:.2f} ({x[max_index]:.2f}, {y[max_index]:.2f})')
-                text.setPos(x[max_index], y[max_index])
+                max_arrow.setIndex(max_index)
+                last_arrow.setIndex(len(x)-1)
+                # text.setText(f'Max: {v[max_index]:.2f} ({x[max_index]:.2f}, {y[max_index]:.2f})')
+                # text.setPos(x[max_index], y[max_index])
 
             self.graph_manager_widget.register_graph(name, graph, _update_graph)
 
     def update_graphs(self, data):
-        for metric_name in ['score', 'variance', *data.metrics]:
+        for metric_name in ['variance', 'score', *data.metrics]:
             self.init_graph(metric_name)
 
         self.graph_manager_widget.update(data)
