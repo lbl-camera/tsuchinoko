44b371722b79fda188458ed3bc4062512360800b
ronpandolfi <ronpandolfi@gmail.com>
2022-06-29 13:28:54 -0700

    implement lock on dataclass

--- tsuchinoko/adaptive/__init__.py
+++ tsuchinoko/adaptive/__init__.py
@@ -1,6 +1,5 @@
-from dataclasses import dataclass, field, asdict
-from collections import defaultdict
-
+from dataclasses import dataclass, field, asdict, fields
+import threading
 
 @dataclass()
 class Data:
@@ -10,31 +9,41 @@ class Data:
     variances: list = field(default_factory=list)
     metrics: dict = field(default_factory=dict)
 
+    def __post_init__(self):
+        self._lock = threading.Lock()
+
     def inject_new(self, data):
-        for datum in data:
-            self.positions.append(datum[0])
-            self.scores.append(datum[1])
-            self.variances.append(datum[2])
-            for metric in datum[3]:  # TODO: handle logical cases
-                if metric not in self.metrics:
-                    self.metrics[metric] = []
-                self.metrics[metric].append(datum[3][metric])
+        with self:
+            for datum in data:
+                self.positions.append(datum[0])
+                self.scores.append(datum[1])
+                self.variances.append(datum[2])
+                for metric in datum[3]:  # TODO: handle logical cases
+                    if metric not in self.metrics:
+                        self.metrics[metric] = []
+                    self.metrics[metric].append(datum[3][metric])
 
     as_dict = asdict
 
     def __getitem__(self, item:slice):
-        print(item)
         return Data(self.dimensionality, self.positions[item], self.scores[item], self.variances[item], {key: value[item] for key, value in self.metrics.items()})
 
     def __len__(self):
         return len(self.positions)
 
     def extend(self, data: 'Data'):
-        self.positions += data.positions
-        self.scores += data.scores
-        self.variances += data.variances
-        for key in self.metrics:
-            self.metrics[key] += data[key]
+        with self:
+            self.positions += data.positions
+            self.scores += data.scores
+            self.variances += data.variances
+            for key in self.metrics:
+                self.metrics[key] += data[key]
+
+    def __enter__(self):
+        self._lock.__enter__()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._lock.__exit__(exc_type, exc_val, exc_tb)
 
 
 class Engine:
