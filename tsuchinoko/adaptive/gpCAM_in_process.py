c664349b81227c0676f8684078f9d2fa40b9d944
ronpandolfi <ronpandolfi@gmail.com>
2022-08-01 14:43:09 -0700

    parameter refactor

--- tsuchinoko/adaptive/gpCAM_in_process.py
+++ tsuchinoko/adaptive/gpCAM_in_process.py
@@ -1,26 +1,77 @@
+from functools import cached_property
+
 import numpy as np
+from pyqtgraph.parametertree.parameterTypes import SimpleParameter, GroupParameter, ListParameter
+
 from gpcam.gp_optimizer import GPOptimizer
-
 from . import Engine, Data
-
-from gpcam.autonomous_experimenter import AutonomousExperimenterGP
 
 
 class GPCAMInProcessEngine(Engine):
 
     def __init__(self, dimensionality, parameter_bounds, hyperparameters, hyperparameter_bounds, **kwargs):
-        self.parameter_bounds = parameter_bounds
-        self.hyperparameters = hyperparameters
-        self.hyperparameter_bounds = hyperparameter_bounds
         self.kwargs = kwargs
         self.optimizer = GPOptimizer(dimensionality, parameter_bounds)
-
         self.optimizer.tell(np.empty((1, dimensionality)), np.empty((1,)), np.empty((1,)))  # we'll wipe this out later; required for initialization
         self.optimizer.init_gp(hyperparameters)
+
+        for i in range(dimensionality):
+            for j, edge in enumerate(['min', 'max']):
+                self.parameters[('bounds', f'axis_{i}_{edge}')] = parameter_bounds[i][j]
+        for i in range(dimensionality+1):
+            for j, edge in enumerate(['min', 'max']):
+                self.parameters[('hyperparameters', f'hyperparameter_{i}_{edge}')] = hyperparameter_bounds[i][j]
+            self.parameters.child('hyperparameters', f'hyperparameter_{i}').setValue(hyperparameters[i], blockSignal=self._set_hyperparameter)
                                # compute_device = compute_device,
                                # gp_kernel_function = self.kernel_func,
                                # gp_mean_function = self.prior_mean_func,
                                # sparse = sparse)
+
+        self.optimizer.points = np.array([])
+        self.optimizer.values = np.array([])
+        self.optimizer.variances = np.array([])
+
+    def reset(self):
+        parameter_bounds = np.asarray([[self.parameters[('bounds', f'axis_{i}_{edge}')]
+                                        for edge in ['min', 'max']]
+                                       for i in range(self.dimensionality)])
+        hyperparameters = np.asarray([self.parameters[('hyperparameters', f'hyperparameter_{i}')]
+                                      for i in range(self.dimensionality+1)])
+
+        self.optimizer = GPOptimizer(self.dimensionality, parameter_bounds)
+        self.optimizer.tell(np.empty((1, self.dimensionality)), np.empty((1,)), np.empty((1,)))  # we'll wipe this out later; required for initialization
+        self.optimizer.init_gp(hyperparameters)
+
+        self.optimizer.points = np.array([])
+        self.optimizer.values = np.array([])
+        self.optimizer.variances = np.array([])
+
+    @cached_property
+    def parameters(self):
+        hyper_parameters = [SimpleParameter(title=f'Hyperparameter #{i+ 1}', name=f'hyperparameter_{i}', type='float')
+                            for i in range(1+self.dimensionality)]
+        hyper_parameters_bounds = [SimpleParameter(title=f'Hyperparameter #{i+ 1} {edge}', name=f'hyperparameter_{i}_{edge}', type='float')
+                            for i in range(1+self.dimensionality) for edge in ['min', 'max']]
+        bounds_parameters = [SimpleParameter(title=f'Axis #{i + 1} {edge}', name=f'axis_{i}_{edge}', type='float')
+                                 for i in range(self.dimensionality) for edge in ['min', 'max']]
+        func_parameters = [ListParameter(title='Method', name='method', values=['global', 'local', 'hgdl']),
+                           ListParameter(title='Acquisition Function', name='acquisition_function', values=['variance', 'shannon_ig', 'UCB', 'maximum', 'minimum', 'covariance']),
+                           SimpleParameter(title='Queue Length', name='n', value=1, type='int'),
+                           SimpleParameter(title='Population Size (global only)', name='pop_size', value=20, type='int'),
+                           SimpleParameter(title='Tolerance', name='tol', value=1e-6, type='float')]
+
+        # wireup callback-based parameters
+        for param in hyper_parameters:
+            param.sigValueChanged.connect(self._set_hyperparameter)
+
+        parameters = func_parameters + [GroupParameter(name='bounds', title='Axis Bounds', children=bounds_parameters),
+                                        GroupParameter(name='hyperparameters', title='Hyperparameter Bounds', children=hyper_parameters+hyper_parameters_bounds)]
+        return GroupParameter(name='top', children=parameters)
+
+    def _set_hyperparameter(self, parameter, value):
+        self.optimizer.gp_initialized = False  # Force re-initialization
+        self.optimizer.init_gp(np.asarray([self.parameters[('hyperparameters', f'hyperparameter_{i}')]
+                                           for i in range(self.dimensionality+1)]))
 
     @property
     def dimensionality(self):
@@ -30,7 +81,20 @@ class GPCAMInProcessEngine(Engine):
         self.optimizer.tell(data.positions, data.scores, data.variances)
 
     def request_targets(self, position, n, **kwargs):
+        for key in ['acquisition_function', 'method', 'pop_size', 'tol']:
+            kwargs.update({key: self.parameters[key]})
+        kwargs.update({'bounds': np.asarray([[self.parameters[('bounds', f'axis_{i}_{edge}')]
+                                              for edge in ['min', 'max']]
+                                             for i in range(self.dimensionality)])})
         return self.optimizer.ask(position, n, **kwargs)['x']
 
     def train(self):
-        self.optimizer.train(self.hyperparameter_bounds, self.hyperparameters)
+        self.optimizer.train(np.asarray([[self.parameters[('hyperparameters', f'hyperparameter_{i}_{edge}')]
+                                          for edge in ['min', 'max']]
+                                         for i in range(self.dimensionality+1)]),
+                             np.asarray([self.parameters[('hyperparameters', f'hyperparameter_{i}')]
+                                         for i in range(self.dimensionality+1)]))
+
+    def metrics(self):
+        points = np.random.random()
+        return self.optimizer.posterior_covariance(points)
