ba1dce923c373147181741c1f1fb1d64682b766f
ronpandolfi <ronpandolfi@gmail.com>
2022-09-01 12:03:27 -0700

    refactor duplicate code into base

--- tsuchinoko/graphics_items/mixins.py
+++ tsuchinoko/graphics_items/mixins.py
@@ -1,10 +1,8 @@
-from functools import cached_property
+from pyqtgraph import ImageView, PlotWidget
+from qtpy.QtCore import Signal, QObject, QEvent, Qt
+from qtpy.QtWidgets import QAction
 
-import numpy as np
-from pyqtgraph import ImageView, GraphicsObject, PlotWidget
-from qtpy.QtCore import QEvent, QRect, Qt, QPoint, Signal, QObject
-from qtpy.QtGui import QPainterPath, QMouseEvent
-from qtpy.QtWidgets import QMenu, QAction, QApplication
+# TODO: map imageitems into target coordinate domain
 
 
 class RequestRelay(QObject):
@@ -14,15 +12,21 @@ class RequestRelay(QObject):
 request_relay = RequestRelay()
 
 
-class ClickRequester(ImageView):
+class ClickRequesterBase:
     def __init__(self, *args, **kwargs):
-        super(ClickRequester, self).__init__(*args, **kwargs)
+        super(ClickRequesterBase, self).__init__(*args, **kwargs)
 
         self.measure_action = QAction('Queue Measurement at Point')
         self.measure_action.triggered.connect(self.emit_measure_request)
-        self.scene.contextMenu.append(self.measure_action)
+        self._scene().contextMenu.append(self.measure_action)
         self._last_mouse_event_pos = None
-        self.ui.graphicsView.installEventFilter(self)
+        self._install_filter()
+
+    def _install_filter(self):
+        ...
+
+    def _scene(self):
+        ...
 
     def eventFilter(self, obj, ev):
         if ev.type() == QEvent.Type.MouseButtonPress:
@@ -32,6 +36,12 @@ class ClickRequester(ImageView):
         return False
 
 
+class ClickRequester(ClickRequesterBase, ImageView):
+    def _scene(self):
+        return self.scene
+
+    def _install_filter(self):
+        self.ui.graphicsView.installEventFilter(self)
 
     def emit_measure_request(self, *_):
         app_pos = self._last_mouse_event_pos
@@ -40,22 +50,12 @@ class ClickRequester(ImageView):
         request_relay.sigRequestMeasure.emit(local_pos.toTuple())
 
 
-class ClickRequesterPlot(PlotWidget):
-    def __init__(self, *args, **kwargs):
-        super(ClickRequesterPlot, self).__init__(*args, **kwargs)
-
-        self.measure_action = QAction('Queue Measurement at Point')
-        self.measure_action.triggered.connect(self.emit_measure_request)
-        self.sceneObj.contextMenu.append(self.measure_action)
-        self._last_mouse_event_pos = None
+class ClickRequesterPlot(ClickRequesterBase, PlotWidget):
+    def _install_filter(self):
         self.installEventFilter(self)
 
-    def eventFilter(self, obj, ev):
-        if ev.type() == QEvent.Type.MouseButtonPress:
-            if ev.button() == Qt.MouseButton.RightButton:
-                self._last_mouse_event_pos = ev.pos()
-
-        return False
+    def _scene(self):
+        return self.sceneObj
 
     def emit_measure_request(self, *_):
         app_pos = self._last_mouse_event_pos
