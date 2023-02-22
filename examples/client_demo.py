2cba0b9614ea23aff47a0da5cd291abbdea156f1
Ronald Pandolfi <ronpandolfi@gmail.com>
2023-02-22 09:16:16 -0800

    refactor client_demo

--- examples/client_demo.py
+++ examples/client_demo.py
@@ -1,15 +1,7 @@
 """
 Launches a client window; attempts to connect at localhost address
 """
-
-from pyqtgraph import mkQApp
-
-from tsuchinoko.widgets.mainwindow import MainWindow
+import tsuchinoko
 
 if __name__ == '__main__':
-    qapp = mkQApp('Tsuchinoko')
-
-    main_window = MainWindow()
-    main_window.show()
-
-    exit(qapp.exec_())
+    tsuchinoko.launch_client()
