fd559248d286cd11bd43b73577a8808336f34947
Ronald Pandolfi <ronpandolfi@gmail.com>
2023-02-22 09:17:01 -0800

    demo of DynamicColorMultiPlot

--- examples/vector_metric_demo.py
+++ examples/vector_metric_demo.py
@@ -8,7 +8,7 @@ from itertools import count
 from tsuchinoko.adaptive.gpCAM_in_process import GPCAMInProcessEngine
 from tsuchinoko.core import ZMQCore
 from tsuchinoko.execution.simple import SimpleEngine
-from tsuchinoko.graphs.common import Plot, MultiPlot
+from tsuchinoko.graphs.common import Plot, MultiPlot, DynamicColorMultiPlot
 
 # Load data from a jpg image to be used as a luminosity map
 image = np.flipud(np.asarray(Image.open(Path(__file__).parent/'sombrero_pug.jpg')))
@@ -17,17 +17,21 @@ luminosity = np.average(image, axis=2)
 # A counter to track measurement number
 counter = count(1)
 
+def objective(x, y):
+    return -20.0 * np.exp(-0.2 * np.sqrt(0.5 * (x ** 2 + y ** 2))) - np.exp(0.5 * (np.cos(2 * np.pi * x) + np.cos(2 * np.pi * y))) + np.e + 20
+
 
 # Bilinear sampling will be used to effectively smooth pixel edges in source data
 def bilinear_sample(pos):
     i = next(counter)
-    v = np.random.random((3))
+    v = objective(pos[0], np.arange(-20, 20))
     return pos, \
         ndimage.map_coordinates(luminosity, [[pos[1]], [pos[0]]], order=1)[0], \
         1, \
         {'plot_data': v,
          'plot_label': f'Measurement #{i}',
-         'pen': {'color': mkColor(v*255).getRgb(), 'width': int(np.sum(v)*3)}
+         # 'pen': {'width': int(np.max(v)), 'color': np.min(v)}
+         'scalar_color': np.min(v)
          }
 
 
@@ -43,7 +47,7 @@ adaptive = GPCAMInProcessEngine(dimensionality=2,
                                                        (0, 1e5),
                                                        (0, 1e5)])
 
-multiplot_graph_example = MultiPlot(data_key='plot_data', name='Vector Metric Example', label_key='plot_label', pen_key='pen', accumulates=True)
+multiplot_graph_example = DynamicColorMultiPlot(color_scalar_key='scalar_color', data_key='plot_data', name='Vector Metric Example', label_key='plot_label', accumulates=True)
 
 adaptive.graphs.append(multiplot_graph_example)
 
