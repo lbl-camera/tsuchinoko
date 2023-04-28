a00694228c5fd2b9018352833cb0813e4a27d743
Ronald Pandolfi <ronpandolfi@gmail.com>
2023-04-28 14:06:53 -0700

    Add pyqode patch for pyside2

--- tsuchinoko/__init__.py
+++ tsuchinoko/__init__.py
@@ -11,6 +11,7 @@ from ._version import get_versions
 
 __version__ = get_versions()['version']
 
+from . import patches
 from .utils import runengine
 from . import parameters  # registers parameter types
 
