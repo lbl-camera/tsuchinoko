80e2c04d37d31ef10fd0edc345ce1bae671bf874
Ronald Pandolfi <ronpandolfi@gmail.com>
2025-03-31 20:57:39 -0700

    fix editor in pyside6

--- tsuchinoko/patches/pyqode.py
+++ tsuchinoko/patches/pyqode.py
@@ -1,6 +1,8 @@
+import enum
 import struct
 
 from pyqode.core.api.client import JsonTcpClient, comm  # Must be a late import
+from qtpy import QtGui, QtCore, QtWidgets
 
 
 def _read_header(self):
@@ -21,3 +23,16 @@ def _read_header(self):
 
 
 JsonTcpClient._read_header = _read_header
+
+# fix enum promotion in qtpy/pyside6. This looks wacky, but it actually does something.
+for module in [QtCore, QtWidgets, QtGui]:
+    class_names = [name for name in dir(module) if name.startswith("Q")]
+    for class_name in class_names:
+        klass = getattr(module, class_name)
+        attrib_names = [name for name in dir(klass) if name[0].isupper()]
+        for attrib_name in attrib_names:
+            attrib = getattr(klass, attrib_name)
+            if not isinstance(attrib, enum.EnumMeta):
+                continue
+            for name, value in attrib.__members__.items():
+                setattr(klass, name, value)
